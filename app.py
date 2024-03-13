bd18f160b4ec97b9c5937f341746d5b5ff7822c2
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2024-03-13 16:35:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,29 +6,20 @@ st.title('Filtrador de CSV para Analytics')
 uploaded_file = st.file_uploader("Elige un archivo CSV", type="csv")
 
 if uploaded_file is not None:
-    # Asegúrate de que Pandas use el delimitador correcto, en este caso, una coma
+    # Cargar el archivo CSV, asegurándose de saltar las líneas de metadatos y usar el delimitador correcto
     df = pd.read_csv(uploaded_file, skiprows=9, delimiter=',')
 
-    # Mostrar las columnas en un selectbox y permitir al usuario seleccionar una
-    columna_seleccionada = st.selectbox('Selecciona la columna que quieres conservar:', df.columns)
-    
-    # Mostrar DataFrame completo antes de la selección para depuración
+    # Mostrar una vista previa de todos los datos cargados
     st.write("Vista previa de los datos cargados:")
     st.dataframe(df)
     
-    # Filtrar el DataFrame para conservar solo la columna seleccionada
-    df_filtrado = df[[columna_seleccionada]]
-    
-    # Mostrar los datos filtrados con solo la columna seleccionada
-    st.write(f"Aquí están tus datos filtrados en la columna '{columna_seleccionada}':")
-    st.dataframe(df_filtrado)
-    
-    # Convertir el DataFrame filtrado a CSV para permitir la descarga
-    csv = df_filtrado.to_csv(index=False).encode('utf-8')
+    # Opción para descargar el archivo CSV originalmente cargado
+    # Si necesitas realizar algún filtrado u operación antes de la descarga, puedes ajustar el DataFrame aquí
+    csv = df.to_csv(index=False).encode('utf-8')
     
     st.download_button(
-        label="Descargar archivo filtrado",
+        label="Descargar archivo CSV",
         data=csv,
-        file_name=f'archivo_filtrado_{columna_seleccionada}.csv',
+        file_name='datos_cargados.csv',
         mime='text/csv',
     )
