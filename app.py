0fa4d208eaeeb7616a3975674e6ac09ff565be7f
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2024-03-13 16:29:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,34 @@
 import streamlit as st
 import pandas as pd
 
-# Título de la aplicación en Streamlit
 st.title('Filtrador de CSV para Analytics')
 
-# Widget de carga de archivo CSV
 uploaded_file = st.file_uploader("Elige un archivo CSV", type="csv")
 
-# Procesamiento después de que el archivo es cargado
 if uploaded_file is not None:
-    # Lectura del archivo CSV, saltando las primeras 9 líneas de metadatos
-    df = pd.read_csv(uploaded_file, skiprows=9)
-    
-    # Permitir al usuario seleccionar la columna de interés a través de un dropdown
+    # Asegúrate de que Pandas use el delimitador correcto, en este caso, una coma
+    df = pd.read_csv(uploaded_file, skiprows=9, delimiter=',')
+
+    # Mostrar las columnas en un selectbox y permitir al usuario seleccionar una
     columna_seleccionada = st.selectbox('Selecciona la columna que quieres conservar:', df.columns)
     
-    # Filtrar filas que contengan '/es/videos/' o '/en/videos/' en la columna seleccionada
-    filtro = '/es/videos/|/en/videos/'
-    df_filtrado = df[df[columna_seleccionada].str.contains(filtro, na=False)]
+    # Mostrar DataFrame completo antes de la selección para depuración
+    st.write("Vista previa de los datos cargados:")
+    st.dataframe(df)
     
-    # Conservar solo la columna seleccionada por el usuario
-    df_filtrado = df_filtrado[[columna_seleccionada]]
+    # Filtrar el DataFrame para conservar solo la columna seleccionada
+    df_filtrado = df[[columna_seleccionada]]
     
-    # Mostrar los datos filtrados en la interfaz de Streamlit
-    st.write("Aquí están tus datos filtrados:")
+    # Mostrar los datos filtrados con solo la columna seleccionada
+    st.write(f"Aquí están tus datos filtrados en la columna '{columna_seleccionada}':")
     st.dataframe(df_filtrado)
     
     # Convertir el DataFrame filtrado a CSV para permitir la descarga
     csv = df_filtrado.to_csv(index=False).encode('utf-8')
     
-    # Botón de descarga para el archivo CSV filtrado
     st.download_button(
         label="Descargar archivo filtrado",
         data=csv,
-        file_name='archivo_filtrado.csv',
+        file_name=f'archivo_filtrado_{columna_seleccionada}.csv',
         mime='text/csv',
     )
