1f911594fd6879411fdf1b478dc426c71137d759
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2024-03-13 16:13:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,37 +9,30 @@ uploaded_file = st.file_uploader("Elige un archivo CSV", type="csv")
 
 # Procesamiento después de que el archivo es cargado
 if uploaded_file is not None:
-    # Lectura del archivo CSV
-    df = pd.read_csv(uploaded_file)
+    # Lectura del archivo CSV, saltando las primeras 9 líneas de metadatos
+    df = pd.read_csv(uploaded_file, skiprows=9)
     
-    # Si el archivo CSV contiene al menos una columna, proceder
-    if len(df.columns) > 0:
-        # Permitir al usuario seleccionar la columna de interés a través de un dropdown
-        columna_seleccionada = st.selectbox('Selecciona la columna que quieres conservar:', df.columns)
-        
-        # Filtrar filas que contengan '/es/videos/' o '/en/videos/' en la columna seleccionada
-        filtro = '/es/videos/|/en/videos/'
-        df_filtrado = df[df[columna_seleccionada].str.contains(filtro, na=False)]
-        
-        # Conservar solo la columna seleccionada por el usuario
-        df_filtrado = df_filtrado[[columna_seleccionada]]
-        
-        # Mostrar los datos filtrados en la interfaz de Streamlit
-        st.write("Aquí están tus datos filtrados:")
-        st.dataframe(df_filtrado)
-        
-        # Convertir el DataFrame filtrado a CSV para permitir la descarga
-        csv = df_filtrado.to_csv(index=False).encode('utf-8')
-        
-        # Botón de descarga para el archivo CSV filtrado
-        st.download_button(
-            label="Descargar archivo filtrado",
-            data=csv,
-            file_name='archivo_filtrado.csv',
-            mime='text/csv',
-        )
-    else:
-        st.error("El archivo CSV parece estar vacío. Por favor, carga un archivo con datos.")
-else:
-    st.info("Por favor, carga un archivo CSV para comenzar.")
-
+    # Permitir al usuario seleccionar la columna de interés a través de un dropdown
+    columna_seleccionada = st.selectbox('Selecciona la columna que quieres conservar:', df.columns)
+    
+    # Filtrar filas que contengan '/es/videos/' o '/en/videos/' en la columna seleccionada
+    filtro = '/es/videos/|/en/videos/'
+    df_filtrado = df[df[columna_seleccionada].str.contains(filtro, na=False)]
+    
+    # Conservar solo la columna seleccionada por el usuario
+    df_filtrado = df_filtrado[[columna_seleccionada]]
+    
+    # Mostrar los datos filtrados en la interfaz de Streamlit
+    st.write("Aquí están tus datos filtrados:")
+    st.dataframe(df_filtrado)
+    
+    # Convertir el DataFrame filtrado a CSV para permitir la descarga
+    csv = df_filtrado.to_csv(index=False).encode('utf-8')
+    
+    # Botón de descarga para el archivo CSV filtrado
+    st.download_button(
+        label="Descargar archivo filtrado",
+        data=csv,
+        file_name='archivo_filtrado.csv',
+        mime='text/csv',
+    )
