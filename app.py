c7a449a7bb0a5a6c24a2c61bec4f7f3eed615d5f
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2024-03-13 17:05:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import re  # Para expresiones regulares
-from io import BytesIO  # Para manejar la salida en memoria
+import re
+from io import BytesIO
 
 st.title('Filtrador de CSV para Analytics')
 
@@ -12,9 +12,16 @@ uploaded_file = st.file_uploader("Elige un archivo CSV", type="csv")
 numero_inicial = st.number_input("Número inicial", min_value=0, value=0, step=1, format="%d")
 numero_final = st.number_input("Número final", min_value=0, value=1000000, step=1, format="%d")
 
+# Solicitar al usuario que seleccione el idioma para el filtrado o 'Todos' para no filtrar
+opcion_idioma = st.selectbox('Selecciona el idioma para filtrar (o Todos para no filtrar):', ['Todos', '/es/', '/en/'])
+
 if uploaded_file is not None and numero_inicial < numero_final:
     # Cargar el archivo CSV, asegurándose de saltar las líneas de metadatos y usar el delimitador correcto
     df = pd.read_csv(uploaded_file, skiprows=9, delimiter=',')
+
+    # Filtrar por idioma si es necesario
+    if opcion_idioma != 'Todos':
+        df = df[df['Page path and screen class'].str.startswith(opcion_idioma)]
 
     # Función para filtrar las filas basadas en el rango de números dentro de las URLs
     def filtrar_por_rango(df, col, inicio, fin):
