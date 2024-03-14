aebe3f019ccfceb8e991195baa48430a547b3902
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2024-03-13 17:21:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ from io import BytesIO
 # Configuración de la página
 st.set_page_config(page_title="Metalizer", page_icon=":metal:", layout="wide")
 
-# Estilos personalizados
+# Mostrar el logo de Metatube
+logo_url = "https://www.metatube.com/assets/layout/css/img/LogoFooter.svg"
+st.image(logo_url, width=449)
+
+# Estilos personalizados y título grande
 st.markdown("""
     <style>
     .big-font {
@@ -15,31 +19,28 @@ st.markdown("""
     }
     </style>
     """, unsafe_allow_html=True)
-
 st.markdown('<p class="big-font">Metalizer</p>', unsafe_allow_html=True)
+
 st.write("Una herramienta para filtrar y analizar tus datos de manera metálica.")
 
-# Columnas para inputs
+# Solicitar al usuario que cargue un archivo CSV y seleccione opciones
 col1, col2, col3 = st.columns(3)
-
 with col1:
     uploaded_file = st.file_uploader("Elige un archivo CSV", type="csv")
-
 with col2:
     numero_inicial = st.number_input("Número inicial", min_value=0, value=0, step=1, format="%d")
-
 with col3:
     numero_final = st.number_input("Número final", min_value=0, value=1000000, step=1, format="%d")
 
-# Columna para selección de idioma
 opcion_idioma = st.selectbox('Selecciona el idioma para filtrar (o Todos para no filtrar):', ['Todos', '/es/', '/en/'])
 
+# Función principal para filtrar los datos
 if uploaded_file is not None and numero_inicial < numero_final:
     df = pd.read_csv(uploaded_file, skiprows=9, delimiter=',')
+
     if opcion_idioma != 'Todos':
         df = df[df['Page path and screen class'].str.startswith(opcion_idioma)]
-
-    # Filtrar por rango
+    
     def filtrar_por_rango(df, col, inicio, fin):
         patron = re.compile(r'/videos/(\d+)/')
         def coincide_con_rango(url):
@@ -49,7 +50,7 @@ if uploaded_file is not None and numero_inicial < numero_final:
                 return inicio <= numero <= fin
             return False
         return df[df[col].apply(coincide_con_rango)]
-
+    
     df_filtrado = filtrar_por_rango(df, "Page path and screen class", numero_inicial, numero_final)
     
     st.write("Vista previa de los datos filtrados:")
